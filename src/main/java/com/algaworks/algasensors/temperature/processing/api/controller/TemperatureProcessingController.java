8a311aa9cef8ce8f0750f633f12541bf7c5e10a5
lucianolucas <luciano.lucas@bllc.com.br>
2025-06-08 20:23:10 -0300

    12.14. Consumindo mensagens

--- src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
+++ src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -5,6 +5,7 @@ import com.algaworks.algasensors.temperature.processing.common.IdGenerator;
 import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -50,7 +51,14 @@ public class TemperatureProcessingController {
         String routingKey = "";
         Object messagePayload = temperatureLogOutput;
 
-        rabbitTemplate.convertAndSend(exchange, routingKey, messagePayload);
+        MessagePostProcessor messagePostProcessor =
+                message -> {
+                    message.getMessageProperties().setHeader("sensorId", temperatureLogOutput.getSensorId().toString());
+                    message.getMessageProperties().setHeader("outro", "Outro qualquer");
+                    return message;
+                };
+
+        rabbitTemplate.convertAndSend(exchange, routingKey, messagePayload, messagePostProcessor);
 
     }
 
